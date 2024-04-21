8c8be231b99ad2c5c09bccc8ee5967aef3d494f6
vartikus1337 <pasha_klevtsov@vk.com>
2024-04-21 21:39:46 +0300

    delete in code example HomeView

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,10 @@
 <script setup>
 import Header from "@/components/TheHeader.vue";
-import HomeView from "@/pages/HomeView.vue";
 </script>
 
 <template>
   <Header></Header>
-  <!-- <HomeView></HomeView> -->
   <RouterView />
 </template>
 
-<style>
-
-</style>
+<style></style>
